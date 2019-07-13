ac89261d050ca96e1f1df2758a3c44b19cd7046a
promter11 <promter11@gmail.com>
2019-07-13 14:45:44 +0400

    Final commit

---
+++ app/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.header-list__link').click(function(event) {
+        event.preventDefault();
+
+        $('html').animate({scrollTop: $($(this).attr('href')).offset().top}, 1500);
+    });
+
+    $('.review-square-block__square').click(function(event) {
+        event.preventDefault();
+
+        $('.review-square-block__square').css('background-color', "#fff");
+        $('.review-block').hide();
+
+        $(this).css('background-color', "#848789");
+        $($(this).attr('href')).fadeIn();
+    });
+});
